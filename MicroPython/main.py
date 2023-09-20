16cf6a055f481c6cd67107ea1dc02d6c125926c9
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-09-20 17:10:10 +0000

    python ver

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -6,8 +6,11 @@ This module is a Micro:bit MicroPython program
 
 from microbit import *
 
+display.clear()
+sleep(1)
 
-display.scroll("A = " +=str(3*5))
+
+display.scroll("A = " + str(3 * 5))
 
 
-display.scroll("P = " += str(3*2 + 5*2))
+display.scroll("P = " + str(3 * 2 + 5 * 2))
