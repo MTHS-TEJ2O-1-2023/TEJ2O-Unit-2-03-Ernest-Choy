ce39bd6fd71d6f1801f435fe727232ffe3244024
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-09-20 20:25:07 -0400

    A and P

--- main.ts
+++ main.ts
@@ -9,5 +9,7 @@ basic.clearScreen()
 basic.pause(1000)
 
 basic.showString("AREA WOULD BE =" + (3 * 5).toString())
+
 basic.pause(100)
+
 basic.showString("PERIMETER WOULD BE =" + (3 * 2 + 5 * 2 ).toString())
